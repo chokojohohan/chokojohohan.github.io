be0e08b88eb555a653465ff9bb96df81fe6d52ab
PP Various <ippepe92@gmail.com>
2025-05-11 11:37:01 +0900

    動画のモダール表示とその他の修正

--- javascript/script.js
+++ javascript/script.js
@@ -147,3 +147,16 @@ $(window).scroll(function () {
 $(window).on('load', function () {
 	fadeAnime();/* アニメーション用の関数を呼ぶ*/
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+
+$(".video-open").modaal({
+	type: 'video',
+	overlay_close:true,//モーダル背景クリック時に閉じるか
+	background: '#4373c1', // 背景色
+	overlay_opacity:0.8, // 透過具合
+	before_open:function(){// モーダルが開く前に行う動作
+		$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+	},
+	after_close:function(){// モーダルが閉じた後に行う動作
+		$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+	}
+});
